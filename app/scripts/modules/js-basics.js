4b301b1428958a0e1c182c93383dcfe1d7c687ee
XavierDEBUF <x.debuf@gmail.com>
2015-10-20 12:45:34 +0200

    2 premier test xavier DEBUF
    Deux premier tests ok.

--- app/scripts/modules/js-basics.js
+++ app/scripts/modules/js-basics.js
@@ -1,21 +1,44 @@
-(function(global) {
+(function(global){
     'use strict';
 
     var JS_BASICS={};
 
     JS_BASICS.isNumberEven = function(i) {
         // i will be an integer.
+        return(i%2===0);
+
         // Return true if it's even, and false if it isn't.
+
     };
 
     JS_BASICS.getFileExtension = function(str) {
         // str will be a string, but it may not have a file extension.
         // Return the file extension (with no period) if it has one, otherwise false
+var re = /(?:\.([^.]+))?$/;
+
+
+var ext = re.exec(str)[1];
+if(ext===undefined)
+  return(false);
+else
+return(ext);
     };
 
     JS_BASICS.longestString = function(arr) {
-        // arr will be an array.
-        // Return the longest string in the array
+      var strlg;
+      var regex = new RegExp(/([^A-Za-z0-9\-])/);
+      for(var i= 0; i < arr.length; i++)
+{
+  if (regex.test (arr[i])) {
+
+          return false;
+  }else {
+        if (arr[i].length>arr[i-1].length){
+          strlg=arr[i];
+        }
+}
+        return strlg
+        }
     };
 
     JS_BASICS.reverseString = function(str) {
