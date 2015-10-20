5e37bae41597e3a57bd48a461ae10cb8e06f7e99
XavierDEBUF <x.debuf@gmail.com>
2015-10-20 21:59:46 +0200

    debut de resolution 3e function

--- app/scripts/modules/js-basics.js
+++ app/scripts/modules/js-basics.js
@@ -14,31 +14,39 @@
     JS_BASICS.getFileExtension = function(str) {
         // str will be a string, but it may not have a file extension.
         // Return the file extension (with no period) if it has one, otherwise false
-var re = /(?:\.([^.]+))?$/;
+      var re = /(?:\.([^.]+))?$/;
 
 
-var ext = re.exec(str)[1];
-if(ext===undefined)
-  return(false);
-else
-return(ext);
+      var ext = re.exec(str)[1];
+      if(ext===undefined)
+        return(false);
+      else
+      return(ext);
     };
 
     JS_BASICS.longestString = function(arr) {
-      var strlg;
+      var st=0;
+      var tab;
+      var j;
       var regex = new RegExp(/([^A-Za-z0-9\-])/);
-      for(var i= 0; i < arr.length; i++)
-{
-  if (regex.test (arr[i])) {
 
-          return false;
-  }else {
-        if (arr[i].length>arr[i-1].length){
-          strlg=arr[i];
+      for(var i= 0; i < arr.length; i++)
+      {
+        if (!regex.test(arr[i])) {
+          tab=arr[i];
         }
-}
-        return strlg
-        }
+          else{
+            tab='';
+          }
+//arr[i].split('')
+              if (tab.length>st){
+                st=tab.length;
+                j=tab;
+              }
+            }
+
+
+return j;
     };
 
     JS_BASICS.reverseString = function(str) {
